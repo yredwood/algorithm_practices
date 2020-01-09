16d1efc7133cf12f5571e051c1194667bcdafef6
mike <mike@lang.aitricsdev.com>
2020-01-10 03:08:04 +0900

    sdf

--- protection/_old.py
+++ protection/_old.py
@@ -13,19 +13,19 @@ class Protection():
         #assert len(self.p) == D
         
         self.p_T = []
+        self.cnt = 0
         for w in range(self.W):
+            ver = ''.join([p[w] for p in prt_list])
+            if '0'*self.K in ver or '1'*self.K in ver:
+                self.cnt+=1
             self.p_T.append(
-                ''.join([p[w] for p in prt_list])
+                ver
             )
         #assert len(self.p_T) == W
         # a: 0, b: 1
 
     def count_pass(self):
-        cnt = 0
-        for w in range(len(self.p_T)):
-            if '0'*self.K in self.p_T[w] or '1'*self.K in self.p_T[w]:
-                cnt+=1
-        return cnt
+        return self.cnt
 
 
 def argsort(seq):
@@ -76,6 +76,7 @@ if __name__=='__main__':
     sys.stdin = open('newsample.txt', 'r')
     T = int(input())
     for test_case in range(1, T+1):
+        #t0 = time.time()
         D, W, K = map(int, input().split(' '))
         
         prt_list = []
@@ -93,14 +94,13 @@ if __name__=='__main__':
         #print ('{} / {}'.format(p.count_pass(), p.W))
         
         for s in range(1, K):
-            t0 = time.time()
             result = step(prt_list, K, s)
             if result:
                 break
-            print (s, time.time() - t0)
         if not result:
             s = K
 
+        #print (s, time.time() - t0)
         print ('#{} {}'.format(test_case, s))
 
 
