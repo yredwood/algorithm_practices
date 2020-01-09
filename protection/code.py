f619e0e3da9327af5bf44fc696a9cea142b41f72
mike <mike@lang.aitricsdev.com>
2020-01-09 19:24:24 +0900

    fix

--- protection/code.py
+++ protection/code.py
@@ -3,22 +3,21 @@ import itertools
 
 
 class Protection():
-    def __init__(self, prt_list, D, W, K):
+    def __init__(self, prt_list, K):
         
-        self.D = D
-        self.W = W
+        self.D = len(prt_list)
+        self.W = len(prt_list[0])
         self.K = K
         self.p = prt_list
         #assert len(self.p) == D
         
         self.p_T = []
-        for w in range(W):
+        for w in range(self.W):
             self.p_T.append(
                 ''.join([p[w] for p in prt_list])
             )
         #assert len(self.p_T) == W
         # a: 0, b: 1
-
 
     def count_pass(self):
         cnt = 0
@@ -27,8 +26,11 @@ class Protection():
                 cnt+=1
         return cnt
 
-def get_priority_idx(prt_list, D, W, K):
-    p = Protection(prt_list, D, W, K)
+def get_priority_idx(prt_list, K):
+    D = len(prt_list)
+    W = len(prt_list[0])
+
+    p = Protection(prt_list, K)
     control_cnt = p.count_pass()
     
     # to a
@@ -36,13 +38,13 @@ def get_priority_idx(prt_list, D, W, K):
     for d in range(len(prt_list)):
         control_prt = prt_list.copy()
         control_prt[d] = '0'*W
-        pc = Protection(control_prt, D, W, K)
+        pc = Protection(control_prt, K)
         score = pc.count_pass() - control_cnt
         scores_a.append(score)
 
         control_prt = prt_list.copy()
         control_prt[d] = '1'*W
-        pc = Protection(control_prt, D, W, K)
+        pc = Protection(control_prt, K)
         score = pc.count_pass() - control_cnt
         scores_b.append(score)
         
@@ -51,10 +53,12 @@ def get_priority_idx(prt_list, D, W, K):
 def argsort(seq):
     return sorted(range(len(seq)), key=seq.__getitem__)
             
-def step(prt_list, D, W, K, n_step, scores_a, scores_b):
+def step(prt_list, K, n_step, scores_a, scores_b):
+    D = len(prt_list)
+    W = len(prt_list[0])
     
-    a_list = [i+1 for i in range(len(prt_list))]
-    b_list = [-i-1 for i in range(len(prt_list))]
+    a_list = [i+1 for i in range(D)]
+    b_list = [-i-1 for i in range(D)]
     pairs = itertools.combinations(a_list + b_list, n_step)
     
     scores = []; pair_list = []
@@ -98,7 +102,7 @@ def step(prt_list, D, W, K, n_step, scores_a, scores_b):
             else:
                 new_prt[-t-1] = '1'*W
 
-        p = Protection(new_prt, D, W, K)
+        p = Protection(new_prt, K)
         score = p.count_pass()
         if score == W:
             return True
@@ -108,7 +112,7 @@ def step(prt_list, D, W, K, n_step, scores_a, scores_b):
 
 if __name__=='__main__':
 
-    sys.stdin = open('newsample.txt', 'r')
+    sys.stdin = open('sample_input.txt', 'r')
     T = int(input())
     for test_case in range(1, T+1):
 
@@ -118,16 +122,16 @@ if __name__=='__main__':
         for d in range(D):
             prt_list.append(input().replace(' ', ''))
 
-        p = Protection(prt_list, D, W, K)
+        p = Protection(prt_list, K)
         if p.count_pass() == W:
             print ('#{} {}'.format(test_case, 0))
             continue
 
-        scores_a, scores_b = get_priority_idx(prt_list, D, W, K)
+        scores_a, scores_b = get_priority_idx(prt_list, K)
         #print ('{} / {}'.format(p.count_pass(), p.W))
         
         for s in range(1, K+1):
-            result = step(prt_list, D, W, K, s, scores_a, scores_b)
+            result = step(prt_list, K, s, scores_a, scores_b)
             if result:
                 break
         if not result:
